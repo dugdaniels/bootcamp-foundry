aff8a4d5bd8a7db74c6d50ff35fe62768404d5d9
Corey Daniels <c.daniels@alkime.co>
2023-01-26 10:23:57 -0500

    Complete UserAccountMagicNumber

---
+++ src/UserAccountMagicNumber.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+error InvalidMagicNumber(uint256 sent);
+error MagicNumberAlreadySetForAddress(uint256 number, address setter);
+
+contract UserAccountMagicNumber {
+    event MagicNumberSet(address indexed setter, uint256 number);
+
+    mapping(address => uint256) private magicNumber;
+
+    function setMagicNumber(uint256 _number) public {
+        if (_number == 0) revert InvalidMagicNumber(_number);
+        if (magicNumber[msg.sender] != 0) revert MagicNumberAlreadySetForAddress(magicNumber[msg.sender], msg.sender);
+
+        magicNumber[msg.sender] = _number;
+        emit MagicNumberSet(msg.sender, _number);
+    }
+
+    function getMagicNumber(address _setter) external view returns (uint256) {
+        return magicNumber[_setter];
+    }
+}
---
+++ test/UserAccountMagicNumber.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Utilities} from "./utils/Utilities.sol";
+import "forge-std/Test.sol";
+
+import {UserAccountMagicNumber} from "../src/UserAccountMagicNumber.sol";
+
+error InvalidMagicNumber(uint256 sent);
+error MagicNumberAlreadySetForAddress(uint256 number, address setter);
+
+contract UserAccountMagicNumberTest is Test {
+    event MagicNumberSet(address indexed setter, uint256 number);
+
+    Utilities internal utils;
+    UserAccountMagicNumber public magicNumber;
+    address[] internal users;
+    address internal alice;
+    address internal bob;
+
+    function setUp() public {
+        utils = new Utilities();
+        users = utils.createUsers(1);
+        alice = users[0];
+
+        magicNumber = new UserAccountMagicNumber();
+    }
+
+    function testSetMagicNumber(uint256 number) public {
+        vm.assume(number > 0);
+        vm.prank(alice);
+        vm.expectEmit(true, false, false, true, address(magicNumber));
+        emit MagicNumberSet(alice, number);
+
+        magicNumber.setMagicNumber(number);
+
+        bytes32 slot = keccak256(abi.encodePacked(abi.encode(alice), uint256(0)));
+        assertEq(vm.load(address(magicNumber), slot), bytes32(number));
+    }
+
+    function testCannotSetMagicNumberToZero() public {
+        vm.prank(alice);
+        vm.expectRevert(abi.encodeWithSelector(InvalidMagicNumber.selector, 0));
+        magicNumber.setMagicNumber(0);
+    }
+
+    function testMagicNumberCanOnlyBeSetOnce() public {
+        vm.startPrank(alice);
+
+        magicNumber.setMagicNumber(1);
+        vm.expectRevert(abi.encodeWithSelector(MagicNumberAlreadySetForAddress.selector, 1, alice));
+        magicNumber.setMagicNumber(2);
+
+        vm.stopPrank();
+    }
+
+    function testGetMagicNumber(uint256 number) public {
+        bytes32 slot = keccak256(abi.encodePacked(abi.encode(alice), uint256(0)));
+        vm.store(address(magicNumber), slot, bytes32(number));
+
+        assertEq(magicNumber.getMagicNumber(alice), number);
+    }
+}
